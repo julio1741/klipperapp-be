cbe69b44db5836bbf755bbc9acc95ce11f3507f1
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-06-16 15:25:34 -0400

    Add build_queue and show_queue actions to OrganizationsController and update routes

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class OrganizationsController < ApplicationController
       before_action :set_organization, only: [:show, :update, :destroy]
-      before_action :authorize_request, only: [:clean]
+      before_action :authorize_request, only: [:clean, :build_queue]
 
       def index
         if params[:slug].present?
@@ -49,6 +49,24 @@ module Api
         User.set_users_stand_by
         Rails.cache.clear
         render json: { message: 'Entities cleaned successfully' }, status: :ok
+      end
+
+      def build_queue
+        assign_service = UserQueueService.new(
+          organization_id: @current_user.organization_id,
+          branch_id: @current_user.branch_id,
+          role_name: "agent")
+        assign_service.build_queue
+        render json: { message: 'Build queue sended' }, status: :ok
+      end
+
+      def show_queue
+        assign_service = UserQueueService.new(
+          organization_id: @current_user.organization_id,
+          branch_id: @current_user.branch_id,
+          role_name: "agent")
+        users = assign_service.queue
+        render json: users, status: :ok
       end
 
       private
