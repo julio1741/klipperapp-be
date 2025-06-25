c3648aafcf7b8e760ceeaf863df14d5474ca96f8
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-06-24 22:36:08 -0400

    Remove attribute filtering from JSON responses in PaymentService for user and attendance data

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -20,9 +20,9 @@ class PaymentService
       amount_to_pay = total_earnings - total_expenses
 
       {
-        user: user.as_json(only: [:id, :name, :email]),
-        finished_attendances: finished_attendances.as_json(only: [:id, :status, :user_amount, :start_attendance_at, :end_attendance_at]),
-        other_attendances: other_attendances.as_json(only: [:id, :status, :user_amount, :start_attendance_at, :end_attendance_at]),
+        user: user.as_json,
+        finished_attendances: finished_attendances.as_json,
+        other_attendances: other_attendances.as_json,
         earnings: total_earnings,
         expenses: total_expenses,
         amount_to_pay: amount_to_pay
