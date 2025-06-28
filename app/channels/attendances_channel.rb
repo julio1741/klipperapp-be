40c919cbd788f1f08b0ae841c2621e2b922a0891
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-06-27 23:46:16 -0400

    Remove commented-out AttendancesChannel class and its methods

--- app/channels/attendances_channel.rb
+++
@@ -1,9 +0,0 @@
-# class AttendancesChannel < ApplicationCable::Channel
-#   def subscribed
-#     stream_from "attendances"
-#   end
-#
-#   def unsubscribed
-#     # Any cleanup needed when channel is unsubscribed
-#   end
-# end
