8b259aab1933afb0697014068c72a9e43194b153
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-07-03 12:23:08 -0400

    Fix child_attendances serializer reference and add ChildAttendanceSerializer

--- app/serializers/child_attendance_serializer.rb
+++ app/serializers/child_attendance_serializer.rb
@@ -1,8 +1,7 @@
-class AttendanceSerializer < ActiveModel::Serializer
+class ChildAttendanceSerializer < ActiveModel::Serializer
   attributes :id, :status, :date, :time, :discount, :extra_discount, :user_amount, :organization_amount, :total_amount, :start_attendance_at, :end_attendance_at
 
   belongs_to :attended_by_user, serializer: UserSerializer
   belongs_to :profile
   has_many :services
-  has_many :child_attendances, serializer: ChildAttendanceSerializer
 end
