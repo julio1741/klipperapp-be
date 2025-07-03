8e5fd9b41381b4031b12044b30ec0cfd47805a0c
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-07-03 12:42:28 -0400

    Refactor AttendanceSerializer and ChildAttendanceSerializer attributes for improved readability

--- app/serializers/child_attendance_serializer.rb
+++ app/serializers/child_attendance_serializer.rb
@@ -1,5 +1,8 @@
 class ChildAttendanceSerializer < ActiveModel::Serializer
-  attributes :id, :status, :date, :time, :discount, :extra_discount, :user_amount, :organization_amount, :total_amount, :tip_amount, :start_attendance_at, :end_attendance_at
+  attributes :id, :status, :date, :time, :discount, :extra_discount,
+    :user_amount, :organization_amount, :total_amount, :tip_amount,
+    :start_attendance_at, :end_attendance_at, :attended_by, :branch_id, :organization_id,
+    :profile_id, :payment_methodm, :created_at, :updated_at
 
   belongs_to :attended_by_user, serializer: UserSerializer
   belongs_to :profile
