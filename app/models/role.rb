e77227a4f72cab70f20066b2412d33182306d8ec
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-07-04 21:55:01 -0400

    Make organization and branch associations optional in Role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,7 @@
 class Role < ApplicationRecord
   include Filterable
-  belongs_to :organization
-  belongs_to :branch
+  belongs_to :organization, optional: true
+  belongs_to :branch, optional: true
 
   has_many :users, dependent: :nullify
 
