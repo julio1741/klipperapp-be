f40ebcec84be3860cc2728a2fef39f689f1a48f6
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-07-04 23:42:25 -0400

    Add slug generation on organization creation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,14 @@
 class Organization < ApplicationRecord
   include Filterable
+
+  after_create :set_slug_from_name
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :slug, uniqueness: { case_sensitive: false }, allow_nil: true
+
+  private
+
+  def set_slug_from_name
+    update_column(:slug, name.to_s.parameterize)
+  end
 end
