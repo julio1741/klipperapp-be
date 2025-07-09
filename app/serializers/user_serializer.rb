6c9f53d3e0ac642a81eb60088ed8226f848925f2
Julio Baptista <julio.baptista@theoptimalpartner.com>
2025-07-08 23:10:20 -0400

    Add work_state attribute to UserSerializer for enhanced user data representation

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone_number, :role_id, :organization_id, :branch_id, :active, :photo_url
+  attributes :id, :name, :email, :phone_number, :role_id,
+  :organization_id, :branch_id, :active, :photo_url, :work_state
 end
